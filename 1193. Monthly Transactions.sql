8b30380ba2d3908c09784386e00f6d41a2cd4ceb
rrajput <rrajput@QT-INNO-MAC0106.local>
2024-04-14 15:44:49 +0530

    1193. Monthly Transactions ITable: Transactions

---
+++ 1193. Monthly Transactions.sql
@@ -0,0 +1,57 @@
+-- 1193. Monthly Transactions ITable: Transactions
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | country       | varchar |
+-- | state         | enum    |
+-- | amount        | int     |
+-- | trans_date    | date    |
+-- +---------------+---------+
+-- id is the primary key of this table.
+-- The table has information about incoming transactions.
+-- The state column is an enum of type ["approved", "declined"].
+ 
+
+-- Write an SQL query to find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount.
+
+-- Return the result table in any order.
+
+-- The query result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Transactions table:
+-- +------+---------+----------+--------+------------+
+-- | id   | country | state    | amount | trans_date |
+-- +------+---------+----------+--------+------------+
+-- | 121  | US      | approved | 1000   | 2018-12-18 |
+-- | 122  | US      | declined | 2000   | 2018-12-19 |
+-- | 123  | US      | approved | 2000   | 2019-01-01 |
+-- | 124  | DE      | approved | 2000   | 2019-01-07 |
+-- +------+---------+----------+--------+------------+
+-- Output: 
+-- +----------+---------+-------------+----------------+--------------------+-----------------------+
+-- | month    | country | trans_count | approved_count | trans_total_amount | approved_total_amount |
+-- +----------+---------+-------------+----------------+--------------------+-----------------------+
+-- | 2018-12  | US      | 2           | 1              | 3000               | 1000                  |
+-- | 2019-01  | US      | 1           | 1              | 2000               | 2000                  |
+-- | 2019-01  | DE      | 1           | 1              | 2000               | 2000                  |
+
+
+
+
+SELECT 
+        SUBSTRING(trans_date,1,7) AS MONTH,
+        country,
+        count(CASE WHEN country='null' THEN 1 ELSE 0 END) AS trans_count,
+        sum((CASE WHEN state='approved' then 1 ELSE 0 END)) AS approved_count,
+        sum(amount) AS trans_total_amount,
+        sum((CASE WHEN state='approved' then 1 ELSE 0 END)*amount) AS approved_total_amount
+FROM  Transactions
+
+GROUP BY month,country  
